e2dc41e940ca16bddbd7dcdf98d1246ed13e69cb
zaki358 <t.yamazaki.1129@gmail.com>
2022-11-21 00:46:19 +0900

    front-page css完了

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,10 @@
 import '../scss/style.scss';
 
-console.log("Hi");
+const pageTopBtn = document.querySelector(".p-footer__btn");
+
+pageTopBtn.addEventListener("click", function () {
+  window.scroll({
+    top: 0,
+    behavior: "smooth"
+  })
+});
