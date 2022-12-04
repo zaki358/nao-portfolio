8f05667ff31606eb74749c6001f69f0316a93eab
zaki358 <t.yamazaki.1129@gmail.com>
2022-12-05 00:40:39 +0900

    grid編集

--- dist/js/index.js
+++ dist/js/index.js
@@ -156,6 +156,39 @@ window.addEventListener('DOMContentLoaded', function () {
   mediaQueryList.addEventListener("change", breakPointFunction);
   //初期化
   breakPointFunction(mediaQueryList);
+
+
+  // const aaa = document.querySelectorAll(".p-production__area--title");
+  // aaa.forEach(el => {
+  //   console.log(el.childElementCount);
+  //   //console.log(el.children[0]);
+  // });
+
+  // const a = document.querySelector("#aaa");
+  // console.log(a);
+  // console.log(a.childElementCount);
+  // let bbb = 0;
+  // for(let i =  0;i < a.childElementCount; i++) {
+  //   //console.log(a.children[i].clientHeight);
+  //   bbb = bbb + a.children[i].clientHeight;
+  // }
+  // //console.log(a.clientHeight);
+  // //console.log(bbb);
+  // //console.log(a.parentElement);
+
+  const productionTitle = document.querySelectorAll(".p-production__area--title");
+  //let innerHeight = 0;
+  productionTitle.forEach(el => {
+    let innerHeight = 0;
+    let count = el.childElementCount;
+    for (let i = 0; i < count; i++) {
+      innerHeight = innerHeight + el.children[i].clientHeight;
+    }
+    if (innerHeight != el.clientHeight) {
+      console.log(innerHeight);
+      el.parentElement.style.gridTemplateRows = innerHeight + 'px';
+    }
+  });
 });
 
 
